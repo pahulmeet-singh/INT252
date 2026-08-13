0c1deca52c31ca59d51c19d8e0e5145b97fbd477
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-08-13 15:11:39 +0530

    first lec

---
+++ 01first.js
@@ -0,0 +1,14 @@
+//write some simple ui for the html file
+// const button = document.createElement('button');
+// button.textContent = 'Click Me';
+// button.style.padding = '10px 20px';
+// button.style.fontSize = '16px';
+// button.style.margin = '20px';
+
+// button.addEventListener('click', () => {
+//     alert('Button was clicked!');
+// });
+
+// document.body.appendChild(button);
+
+console.log("Pahulmeet Singh")
