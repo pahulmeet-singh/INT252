008ba32ccd9020d491b56b48624c20cc65353972
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-08-27 15:57:41 +0530

    new function

--- reactet/src/App.jsx
+++ reactet/src/App.jsx
@@ -1,10 +1,26 @@
 // printing hello world in react
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 function App() {
-  return (
+  const name = "John Doe";
+  const course = "BTech";
+  const sem = 1;
+  return(
     <div>
-      <h1>Hello, World!</h1>
+      <h1>My Intro</h1>
+      <p>Name: {name}</p>
+      <p>Course: {course}</p>
+      <p>Semester: {sem}</p>
     </div>
-  );
+  )
 }
 
 export default App;
