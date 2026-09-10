e7933925a78721aac2540fb7b7e4002469cf28c4
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-10 15:49:41 +0530

    z-20

--- project2/src/components/Navbar.jsx
+++ project2/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <header className="absolute inset-x-0 top-0">
+        <header className="absolute inset-x-0 top-0 z-20">
             <nav 
             aria-label="Primary navigation" 
             className="mx-auto flex max-w-6xl items-center justify-between px-5 py-6 sm:px-8 lg:px-10">
