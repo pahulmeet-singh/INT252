eb0ff160093db3f37bac4fdbaed0aa400e979aef
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-10 15:21:25 +0530

    formatting navbar

--- reactet/src/components/Navbar.jsx
+++ reactet/src/components/Navbar.jsx
@@ -2,7 +2,10 @@ export default function Navbar() {
     return (
         <header className="absolute inset-x-0 top-0">
             <nav 
-            aria-label="Primary navigation"></nav>
+            aria-label="Primary navigation" 
+            className="mx-auto flex max-w-6xl items-center justify-between px-5 py-6 sm:px-8 lg:px-10">
+                
+            </nav>
         </header>
     )
 }
