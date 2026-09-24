8fada3aa6b07f1e754262a2ff0b8c07d60e10ff5
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-24 15:06:09 +0530

    mouseOver event

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -2,11 +2,17 @@ function App(){
   function handleClick(){
     alert("Button Clicked");
   }
+  function handleMouseOver(){
+    alert("Mouse Over");
+  }
   return(
     <div>
       <button onClick={handleClick}>
         Clickme
       </button>
+      <p onMouseOver={handleMouseOver}>
+        Hover over this paragraph.
+      </p>
     </div>
   )
 }
